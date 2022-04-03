65e7e551dafe29ccbbb92043b652a0b536bbe0f7
ApostleABC <blessingarowele@gmail.com>
2022-04-03 17:52:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello Blessing Clinton Arowele, I am in Backend Development Track of WellaHealth Schools!");
+            
+            Console.WriteLine("I am intrested in the Backend Development for the following reasons:");
+            Console.WriteLine("   1.To have an upsurge in my digital skills and career path;");
+            Console.WriteLine("   2.To understand how to communicate with my computer;");
+            Console.WriteLine("   3.To have a grounded understanding and application of some programming languages; and");
+            Console.WriteLine("   4.To learn pragmatic ways of solving real-life problems with softwares.");
+            
+        
+        }
+        }
+    }
